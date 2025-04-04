93773fb4079a689f5bcc9d31b81e5bbdee4f2ccb
ibrokhimov.fr <fr.ibrokhimov@gmail.com>
2025-04-04 00:12:17 -0700

    defining request and response models

---
+++ medtime-time-format-to-table/handler.go
@@ -0,0 +1,147 @@
+package function
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/spf13/cast"
+)
+
+const (
+	botToken        = ""
+	chatID          = ""
+	baseUrl         = "https://api.admin.u-code.io"
+	logFunctionName = "ucode-template"
+	IsHTTP          = true
+	appId           = ""
+)
+
+/*
+Answer below questions before starting the function.
+
+When the function invoked?
+ - table_slug -> AFTER | BEFORE | HTTP -> CREATE | UPDATE | MULTIPLE_UPDATE | DELETE | APPEND_MANY2MANY | DELETE_MANY2MANY
+What does it do?
+- Explain the purpose of the function.(O'zbekcha yozilsa ham bo'ladi.)
+*/
+
+// Request structures
+type (
+	NewRequestBody struct {
+		RequestData HttpRequest `json:"request_data"`
+		Auth        AuthData    `json:"auth"`
+		Data        Data        `json:"data"`
+	}
+
+	HttpRequest struct {
+		Method  string      `json:"method"`
+		Path    string      `json:"path"`
+		Headers http.Header `json:"headers"`
+		Params  url.Values  `json:"params"`
+		Body    []byte      `json:"body"`
+	}
+
+	AuthData struct {
+		Type string                 `json:"type"`
+		Data map[string]interface{} `json:"data"`
+	}
+
+	Request struct {
+		Data map[string]interface{} `json:"data"`
+	}
+
+	Data struct {
+		AppId      string                 `json:"app_id"`
+		Method     string                 `json:"method"`
+		ObjectData map[string]interface{} `json:"object_data"`
+		ObjectIds  []string               `json:"object_ids"`
+		TableSlug  string                 `json:"table_slug"`
+		UserId     string                 `json:"user_id"`
+	}
+
+	FunctionRequest struct {
+		BaseUrl     string  `json:"base_url"`
+		TableSlug   string  `json:"table_slug"`
+		AppId       string  `json:"app_id"`
+		Request     Request `json:"request"`
+		DisableFaas bool    `json:"disable_faas"`
+	}
+)
+
+// Response structures
+type (
+	Datas struct {
+		Data struct {
+			Data struct {
+				Data map[string]interface{} `json:"data"`
+			} `json:"data"`
+		} `json:"data"`
+	}
+
+	ClientApiResponse struct {
+		Data ClientApiData `json:"data"`
+	}
+
+	ClientApiData struct {
+		Data ClientApiResp `json:"data"`
+	}
+
+	ClientApiResp struct {
+		Response map[string]interface{} `json:"response"`
+	}
+
+	Response struct {
+		Status string                 `json:"status"`
+		Data   map[string]interface{} `json:"data"`
+	}
+
+	GetListClientApiResponse struct {
+		Data GetListClientApiData `json:"data"`
+	}
+
+	GetListClientApiData struct {
+		Data GetListClientApiResp `json:"data"`
+	}
+
+	GetListClientApiResp struct {
+		Response []map[string]interface{} `json:"response"`
+	}
+
+	ClientApiUpdateResponse struct {
+		Status      string `json:"status"`
+		Description string `json:"description"`
+		Data        struct {
+			TableSlug string                 `json:"table_slug"`
+			Data      map[string]interface{} `json:"data"`
+		} `json:"data"`
+	}
+
+	ClientApiMultipleUpdateResponse struct {
+		Status      string `json:"status"`
+		Description string `json:"description"`
+		Data        struct {
+			Data struct {
+				Objects []map[string]interface{} `json:"objects"`
+			} `json:"data"`
+		} `json:"data"`
+	}
+
+	ResponseStatus struct {
+		Status string `json:"status"`
+	}
+)
+
+type (
+	Asserts struct {
+		Request  NewRequestBody
+		Response Response
+	}
+
+	FunctionAssert struct{}
+)
